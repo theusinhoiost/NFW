4cc6e65ece91bbd93beacb3389d656a4bdff4af1
Matheus Iost <137854934+theusinhoiost@users.noreply.github.com>
2024-04-17 23:37:35 -0300

    Create AddTablesInPrincipalCard.java

---
+++ noforgetwork/src/main/java/com/nfw/AddTablesInPrincipalCard.java
@@ -0,0 +1,27 @@
+package com.nfw;
+
+import javax.swing.*;
+
+public class AddTablesInPrincipalCard {
+    private void ArrayTables(int priority){
+
+    }
+
+
+    public JScrollPane AddNormalTable(){
+        ArrayTables(0);
+
+        return null;
+    }
+    public JScrollPane AddUrgencyTable(){
+        ArrayTables(1);
+
+        return null;
+    }
+    public JScrollPane AddEmergencyTable(){
+        ArrayTables(2);
+
+        return null;
+    }
+
+}
