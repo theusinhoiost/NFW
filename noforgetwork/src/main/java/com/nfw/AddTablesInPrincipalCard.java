c2ac62fd6533ff51b0638053c599c6013840e71b
Matheus Iost <137854934+theusinhoiost@users.noreply.github.com>
2024-04-24 21:47:32 -0300

    Update AddTablesInPrincipalCard.java

--- noforgetwork/src/main/java/com/nfw/AddTablesInPrincipalCard.java
+++ noforgetwork/src/main/java/com/nfw/AddTablesInPrincipalCard.java
@@ -1,22 +1,26 @@
+package com.nfw;
+
 import javax.swing.*;
-import javax.swing.table.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.*;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
 
 public class AddTablesInPrincipalCard {
     Connection connection = null;
     Statement statement = null;
     ResultSet resultSet = null;
 
-    private List<EventData> fetchEvents(int priorityValues) {
+    private List<EventData> ArrayTables(int priorityValues) {
         List<EventData> events = new ArrayList<>();
         try {
             Properties properties = new Properties();
             properties.load(new FileInputStream("noforgetwork/src/main/resources/db.properties"));
-            String url = properties.getProperty("db.url") + "/events";
+            String url = properties.getProperty("db.url")+ "/events";
             String username = properties.getProperty("db.user");
             String password = properties.getProperty("db.password");
             connection = DriverManager.getConnection(url, username, password);
@@ -28,8 +32,9 @@ public class AddTablesInPrincipalCard {
                 String event = resultSet.getString("event");
                 String date = resultSet.getString("date");
                 int priority = resultSet.getInt("priority");
+                String eventFormatted = event;
                 if (priority == priorityValues) {
-                    EventData eventData = new EventData(event, date, false);
+                    EventData eventData = new EventData(eventFormatted, date, false);
                     events.add(eventData);
                 }
             }
@@ -47,18 +52,18 @@ public class AddTablesInPrincipalCard {
         return events;
     }
 
-    public JScrollPane addNormalTable() {
-        List<EventData> normalEvents = fetchEvents(0);
+    public JScrollPane AddNormalTable() {
+        List<EventData> normalEvents = ArrayTables(0);
         return createTable(normalEvents);
     }
 
-    public JScrollPane addUrgencyTable() {
-        List<EventData> urgencyEvents = fetchEvents(1);
+    public JScrollPane AddUrgencyTable() {
+        List<EventData> urgencyEvents = ArrayTables(1);
         return createTable(urgencyEvents);
     }
 
-    public JScrollPane addEmergencyTable() {
-        List<EventData> emergencyEvents = fetchEvents(2);
+    public JScrollPane AddEmergencyTable() {
+        List<EventData> emergencyEvents = ArrayTables(2);
         return createTable(emergencyEvents);
     }
 
@@ -68,28 +73,17 @@ public class AddTablesInPrincipalCard {
         for (EventData eventData : events) {
             model.addRow(new Object[]{eventData.getEvent(), eventData.getDate(), eventData.isChecked()});
         }
-
         JTable table = new JTable(model) {
             @Override
-            public Class<?> getColumnClass(int column) {
+            public Class getColumnClass(int column) {
                 if (column == 2) return Boolean.class;
                 return String.class;
             }
         };
-
-        // Definindo a largura mínima para a coluna de Checkbox
-        TableColumn checkoutColumn = table.getColumnModel().getColumn(2);
-        checkoutColumn.setMinWidth(50); // Define largura mínima para a coluna de Checkbox
-        checkoutColumn.setMaxWidth(75); // Define uma largura máxima para manter a proporção
-
-        // Se desejar, defina largura mínima para outras colunas
-        TableColumn eventColumn = table.getColumnModel().getColumn(0);
-        eventColumn.setMinWidth(150); // Defina uma largura mínima para a coluna de Evento
-
-        JScrollPane scrollPane = new JScrollPane(table);
         table.setPreferredScrollableViewportSize(new Dimension(450, 200));
+        table.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
+        JScrollPane scrollPane = new JScrollPane(table);
         table.getColumnModel().getColumn(2).setCellEditor(new DefaultCellEditor(new JCheckBox()));
-
         return scrollPane;
     }
 
@@ -100,8 +94,8 @@ public class AddTablesInPrincipalCard {
 
         public EventData(String event, String date, boolean checked) {
             this.event = event;
-            this.checked = checked;
             this.date = date;
+            this.checked = checked;
         }
 
         public String getEvent() {
@@ -129,3 +123,4 @@ public class AddTablesInPrincipalCard {
         }
     }
 }
+
