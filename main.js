3d337002398ec47f6fe63331da8cc81a2b35d69e
Matheus Iost <137854934+theusinhoiost@users.noreply.github.com>
2024-08-13 17:34:40 -0300

    theme and event page
    add theme option;
    add create event;

--- main.js
+++ main.js
@@ -1,41 +1,43 @@
-const path = require('path');
+const { app, BrowserWindow, ipcMain, nativeTheme } = require('electron/main')
+const path = require('node:path')
 
-require('electron-reload')(__dirname, {
-  electron: path.join(__dirname, 'node_modules', '.bin', 'electron'),
-  awaitWriteFinish: true,
-});
-
-// Restante do seu código principal
-const { app, BrowserWindow } = require('electron');
-
-let mainWindow;
-
-function createWindow() {
-  mainWindow = new BrowserWindow({
+function createWindow () {
+  const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true,
-    },
-  });
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
 
-  mainWindow.loadFile('index.html');
-
-  mainWindow.on('closed', function () {
-    mainWindow = null;
-  });
+  win.loadFile('index.html')
 }
 
-app.on('ready', createWindow);
+ipcMain.handle('dark-mode:toggle', () => {
+  if (nativeTheme.shouldUseDarkColors) {
+    nativeTheme.themeSource = 'light'
+  } else {
+    nativeTheme.themeSource = 'dark'
+  }
+  return nativeTheme.shouldUseDarkColors
+})
 
-app.on('window-all-closed', function () {
+ipcMain.handle('dark-mode:system', () => {
+  nativeTheme.themeSource = 'system'
+})
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app.quit();
+    app.quit()
   }
-});
-
-app.on('activate', function () {
-  if (mainWindow === null) {
-    createWindow();
-  }
-});
+})
--- src/scripts/thememodifier.js
+++ src/scripts/thememodifier.js
@@ -1,5 +1,8 @@
-const btnTheme = document.querySelector('');
+const data_bs_theme = document.querySelectorAll('.data-bs-theme');
 
-btnTheme.addEventListener(){
+document.getElementById('toggle-dark-mode').addEventListener('click', async () => {
+    const isDarkMode = await window.darkMode.toggle()
+    document.getElementById('theme-source').innerHTML = isDarkMode ? 'Dark' : 'Light'
+  })
+  
 
-};
